3e0f4ec24548df278e68fd7ff01543c0d8d65cbc
yowari <yowari@users.noreply.github.com>
2022-01-04 01:11:14 +0100

    revert xhome proxy url

--- api/xhome/index.ts
+++ api/xhome/index.ts
@@ -1,6 +1,6 @@
 import { AzureFunction } from '@azure/functions';
 import { proxy } from '../utils/proxy';
 
-const httpTrigger: AzureFunction = proxy('xremote.free.beeceptor.com');
+const httpTrigger: AzureFunction = proxy('uks.gssv-play-prodxhome.xboxlive.com');
 
 export default httpTrigger;
